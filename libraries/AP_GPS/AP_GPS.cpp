320b4e852000f4d24923686c80b727b7372c1421
jam <wetouns@126.com>
2020-08-16 01:48:43 +0800

    修复编译错误问题

--- libraries/AP_GPS/AP_GPS.cpp
+++ libraries/AP_GPS/AP_GPS.cpp
@@ -1002,7 +1002,7 @@ void AP_GPS::send_mavlink_gps_raw(mavlink_channel_t chan)
         hacc * 1000,          // one-sigma standard deviation in mm
         vacc * 1000,          // one-sigma standard deviation in mm
         sacc * 1000,          // one-sigma standard deviation in mm/s
-        0);                   // TODO one-sigma heading accuracy standard deviation
+        0,0);                   // TODO one-sigma heading accuracy standard deviation
 }
 
 #if GPS_MAX_RECEIVERS > 1
